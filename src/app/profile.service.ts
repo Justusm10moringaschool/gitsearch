cc06a2c4c93f8a8a104055c133c723e2329a704d
Justusm10 <justusm10@gmail.com>
2019-12-09 15:42:12 +0300

    add code to service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient }from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
-}
+  private username:string;
+ private client_id = "";
+  private client_secret ="";
+  constructor(private http:HttpClient) { 
+    this.username = "justusm10moringaschool";
+     }
+      getProfileInfo(){
+      return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
+     }
+     getProfileRepos(){
+      return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+    }
+     updateProfile(username:string){
+    this.username = username;
+  }
